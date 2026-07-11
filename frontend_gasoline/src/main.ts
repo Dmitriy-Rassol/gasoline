fd24672e463e045b398b5d3a140a7c8776809f13
Рассоленко Дмитрий <rassoldima@yandex.ru>
2026-07-11 21:50:57 +0300

    fix

--- frontend_gasoline/src/main.ts
+++ frontend_gasoline/src/main.ts
@@ -5,8 +5,10 @@ import App from './App.vue'
 
 const app = createApp(App)
 
+const apikey = import.meta.env.VITE_YANDEX_MAPS_APIKEY || 'decf9c26-db06-4cca-a93f-5ad27dc48b15'
+
 app.use(createYmaps({
-  apikey: import.meta.env.VITE_YANDEX_MAPS_APIKEY,
+  apikey,
 }))
 
 app.mount('#app')
